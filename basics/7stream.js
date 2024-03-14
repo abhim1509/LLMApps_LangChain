daa41f6410d80f1b9f68104fd3eb7e2995209ce2
Abhinav Makker <43211436+abhim1509@users.noreply.github.com>
2024-03-14 16:26:25 +0530

    Update 7stream.js

--- basics/7stream.js
+++ basics/7stream.js
@@ -5,8 +5,8 @@ import { ChatOpenAI } from "@langchain/openai";
 
 const execute = async () => {
   const model = new ChatOpenAI({
-    modelName: "gpt-3.5-turbo-1106",
-    openAIApiKey: "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",
+    modelName: process.env.MODEL,
+    openAIApiKey: process.env.OPENAPIKEY
   });
 
   const prompt = ChatPromptTemplate.fromTemplate(
